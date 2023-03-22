3990d930b2f15f2c4cffa0f574e1c92831ec1767
emmyrunge <erunge@student.touro.edu>
2023-03-22 19:54:15 -0400

    added retrofit and rxjava

--- src/main/java/runge/earthquakes/EarthquakeFrame.java
+++ src/main/java/runge/earthquakes/EarthquakeFrame.java
@@ -1,18 +1,14 @@
 package runge.earthquakes;
 
-import com.google.gson.Gson;
-
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.font.TextAttribute;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+
 
 public class EarthquakeFrame extends JFrame
 {
@@ -20,34 +16,36 @@ public class EarthquakeFrame extends JFrame
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
-
-
-        URL url = new URL("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson");
-        URLConnection connection = url.openConnection();
-        InputStream inputStream = connection.getInputStream();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        Gson gson = new Gson();
-        FeatureCollection featureCollection = gson.fromJson(bufferedReader, FeatureCollection.class);
-
-        JLabel header = new JLabel("Latest Earthquake:");
-        header.setFont(new Font("Serif", Font.ITALIC, 50));
-        Font font = header.getFont();
-        Map<TextAttribute, Object> attributes = new HashMap<>(font.getAttributes());
-        attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
-        header.setFont(font.deriveFont(attributes));
-        header.setVerticalAlignment(JLabel.TOP);
-        header.setHorizontalAlignment(JLabel.CENTER);
-
-        JLabel earthquakePlace = new JLabel(featureCollection.features[0].properties.place);
-        earthquakePlace.setFont(new Font("Serif", Font.ROMAN_BASELINE, 36));
+        JLabel earthquakePlace = new JLabel();
+        earthquakePlace.setFont(new Font("Serif", Font.PLAIN, 36));
         earthquakePlace.setHorizontalAlignment(JLabel.CENTER);
-
-        mainPanel.add(header, BorderLayout.NORTH);
         mainPanel.add(earthquakePlace, BorderLayout.CENTER);
-
         setContentPane(mainPanel);
         setSize(500, 500);
         setTitle("Earthquake Status");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://earthquake.usgs.gov/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .build();
+
+
+        EarthquakeService earthquakeService = retrofit.create(EarthquakeService.class);
+
+        Disposable disposable = earthquakeService.getLatestEarthquakes()
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.newThread())
+                .subscribe(
+                        featureCollection ->
+                        {
+                            String location = featureCollection.features[0].properties.place;
+                            earthquakePlace.setText(location);
+                        },
+
+                        Throwable::printStackTrace
+                );
+
     }
 }
